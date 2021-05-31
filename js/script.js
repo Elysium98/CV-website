fb0c830ef2045b88beb37a707839489fc10f5ce7
Sirbu Alexandru-Paul <justst4r@gmail.com>
2021-05-31 20:41:07 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "bx-sun";
+
+const selectedTheme = sessionStorage.getItem("selected-theme");
+const selectedIcon = sessionStorage.getItem("selected-icon");
+
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "bx-moon" : "bx-sun";
+console.log(getCurrentTheme());
+console.log(getCurrentIcon());
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "bx-moon" ? "add" : "remove"](
+    iconTheme
+  );
+}
+
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  sessionStorage.setItem("selected-theme", getCurrentTheme());
+  sessionStorage.setItem("selected-icon", getCurrentIcon());
+  console.log(getCurrentTheme());
+  console.log(getCurrentIcon());
+});
